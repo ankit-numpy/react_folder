7436201e7c6071141df44cfc66da8f8d5ff53131
Ankit Dixit <88057272+ankit-numpy@users.noreply.github.com>
2025-04-03 16:55:07 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: 'localhost',  // Your system's IP address
+    host: true,  // Your system's IP address
     port: 5173,              // Optional: Change the port if needed
   },
 })
